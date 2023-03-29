0ca37a69ddb8a45146bfd50420d4c67b2ea00f52
javokhir1996 <qochqorov1996@inbox.ru>
2023-03-30 00:02:10 +0500

    changed

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import uiComponents from './ui-components'
 import './assets/main.css'
 
 const app = createApp(App)
+
+uiComponents.map(component => app.component(component.name, component));
 
 app.use(router)
 app.use(store)
